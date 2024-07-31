092fa3503e79057c377b2f03a5be35b25bf68c69
KinalParmar <kinalparmar04@gmail.com>
2024-08-01 01:05:08 +0530

    filter changes2

--- pages/apps/BankDetails.tsx
+++ pages/apps/BankDetails.tsx
@@ -13,27 +13,21 @@ import { Tab } from "@headlessui/react";
 import auth from "../utils/auth";
 
 const BankDetails = ({ model, infoData }: any) => {
-  const [loading, setLoading] = useState(true);
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      setLoading(false);
-    }, 500);
+    const [loading, setLoading] = useState(true);
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            setLoading(false);
+        }, 500);
 
-    return () => clearTimeout(timer);
-  }, []);
+        return () => clearTimeout(timer);
+    }, []);
 
-  return loading ? (
-    <div>
-      <Loader />
-    </div>
-  ) : (
-    <div className="rounded-lg p-10">
-      <form className="space-y-6">
+    return loading ? (
         <div>
             <Loader />
         </div>
     ) : (
-        <div className="p-10 rounded-lg">
+        <div className="rounded-lg p-10">
             <form className="space-y-6">
                 <div>
                     <label htmlFor="BankName" className="block text-black mb-2 dark:text-white">Bank Name</label>
@@ -78,89 +72,7 @@ const BankDetails = ({ model, infoData }: any) => {
                 </div>}
             </form>
         </div>
-
-        <div>
-          <label
-            htmlFor="AccountNumber"
-            className="mb-2 block text-black dark:text-white"
-          >
-            Account Number
-          </label>
-          <input
-            id="AccountNumber"
-            type="text"
-            placeholder="Enter Account Number"
-            className="form-input w-full dark:border-none dark:bg-[#261C16]"
-          />
-        </div>
-
-        <div>
-          <label
-            htmlFor="AccountName"
-            className="mb-2 block text-black dark:text-white"
-          >
-            Account Name
-          </label>
-          <input
-            id="AccountName"
-            type="text"
-            placeholder="Enter Account Name"
-            className="form-input w-full dark:border-none dark:bg-[#261C16]"
-          />
-        </div>
-
-        <div>
-          <label
-            htmlFor="IFSCCode"
-            className="mb-2 block text-black dark:text-white"
-          >
-            IFSC Code
-          </label>
-          <input
-            id="IFSCCode"
-            type="text"
-            placeholder="Enter IFSC Code"
-            className="form-input w-full dark:border-none dark:bg-[#261C16]"
-          />
-        </div>
-
-        <div>
-          <label htmlFor="gridState" className="dark:text-white">
-            Status
-          </label>
-          <select
-            id="gridState"
-            className="form-select text-white-dark dark:border-none dark:bg-[#261C16]"
-          >
-            <option>Selected</option>
-            <option>Submitted</option>
-            <option>approve</option>
-            <option>reject</option>
-          </select>
-        </div>
-
-        <div>
-          <label
-            htmlFor="sessionTimeout"
-            className="mb-2 block text-black dark:text-white"
-          >
-            Message
-          </label>
-          <textarea
-            id="sessionTimeout"
-            placeholder="Enter Message"
-            className="form-input w-full dark:border-none dark:bg-[#261C16]"
-          />
-        </div>
-
-        <div className="flex justify-end">
-          <button type="submit" className="btn btn-primary mt-6 w-fit">
-            Submit Setting
-          </button>
-        </div>
-      </form>
-    </div>
-  );
+    );
 };
 
 export default auth(BankDetails);
